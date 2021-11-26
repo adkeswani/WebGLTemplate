47aaf5879b6ddeab95501b6bc3c71d5f671146d5
Aditya Keswani <adkeswani@gmail.com>
2021-11-26 15:45:07 -0800

    Move out non-template code

--- WebGlTemplate.js
+++ WebGlTemplate.js
@@ -18,6 +18,50 @@ var vertexShaderScript = `
     }
 `
 
+var triangleVertexPositionBuffer;
+var lastTime = 0;
+
+function initBuffers()
+{
+    // Set up vertex buffers
+    triangleVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+    var vertices = [
+        0.0,  0.0,  0.01,
+        20.0, 0.0,  0.01,
+        10.0, 10.0,  0.01
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    triangleVertexPositionBuffer.itemSize = 3;
+    triangleVertexPositionBuffer.numItems = 3;
+}
+
+function drawScene() 
+{
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    glMatrix.mat4.translate(mvMatrix, mvMatrix, glMatrix.vec3.fromValues(1.0, 0.0, 0.0));
+
+    // Send vertex buffer to GL
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.aVertexPosition, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0); 
+    setMatrixUniforms();
+    gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+}
+
+function tick(now)
+{
+    drawScene();
+    if (lastTime != 0) 
+    {
+        var elapsed = now - lastTime;
+    }
+
+    lastTime = now;
+    requestAnimationFrame(tick);
+}
+
 function templateStart() 
 {
     var canvas = document.getElementById("template");
--- modules/WebGlTemplate.js
+++ modules/WebGlTemplate.js
@@ -3,10 +3,6 @@ var gl;
 var mvMatrix = glMatrix.mat4.create();
 var pMatrix = glMatrix.mat4.create();
 
-var triangleVertexPositionBuffer;
-
-var lastTime = 0;
-
 function initGl(canvas) 
 {
     try 
@@ -81,50 +77,9 @@ function initMatrices()
     glMatrix.mat4.identity(mvMatrix);
 }
 
-function initBuffers()
-{
-    // Set up vertex buffers
-    triangleVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    var vertices = [
-        0.0,  0.0,  0.01,
-        20.0, 0.0,  0.01,
-        10.0, 10.0,  0.01
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    triangleVertexPositionBuffer.itemSize = 3;
-    triangleVertexPositionBuffer.numItems = 3;
-}
-
 function setMatrixUniforms() 
 {
     // Send JS matrices to GL
     gl.uniformMatrix4fv(shaderProgram.uPMatrix, false, pMatrix);
     gl.uniformMatrix4fv(shaderProgram.uMVMatrix, false, mvMatrix);
-}
-
-function drawScene() 
-{
-    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    glMatrix.mat4.translate(mvMatrix, mvMatrix, glMatrix.vec3.fromValues(1.0, 0.0, 0.0));
-
-    // Send vertex buffer to GL
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.aVertexPosition, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0); 
-    setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
-}
-
-function tick(now)
-{
-    drawScene();
-    if (lastTime != 0) 
-    {
-        var elapsed = now - lastTime;
-    }
-
-    lastTime = now;
-    requestAnimationFrame(tick);
 }
